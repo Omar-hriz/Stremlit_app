98c043d6011222ecf0ee8b8df897d25ca183bca2
Omar-Hriz <117439952+Omar-hriz@users.noreply.github.com>
2024-11-28 16:13:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,3 @@ st.title("Application Principale")
 
 # Message d'introduction
 st.markdown("Bienvenue dans l'application principale. Utilisez la barre latérale pour naviguer.")
-
-# Navigation
-if st.sidebar.button("Aller à la Page 1"):
-    st.switch_page("page1")
-
-if st.sidebar.button("Aller à la Page 2"):
-    st.switch_page("page2")
